2f9d1eeff0bae6e348416b881fd520c3d61cdfcb
Ian Hess <ian.h.hess@gmail.com>
2023-02-17 15:05:43 -0500

    make clock a class

---
+++ my-second-react-app/src/Clock.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function tick() {
+  root.render(<Clock date={new Date()} />);
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+setInterval(tick, 1000);
+
+root.render(<Clock />);
+
+export default Clock;
